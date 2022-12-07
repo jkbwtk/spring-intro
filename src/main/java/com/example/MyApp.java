0345bf0996e39db37bc512de7037872d16d51951
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-12-07 18:43:35 +0100

    10. Added second instance of MessageService

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -5,9 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyApp {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
+        MessageService messageServiceAux = applicationContext.getBean("messageService", MessageService.class);
 
         System.out.println(messageService.getMessage());
 
