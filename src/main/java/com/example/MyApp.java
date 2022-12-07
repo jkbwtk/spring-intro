b2305dc3c688499260876da27800b957362d3441
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-12-07 19:00:34 +0100

    15. Added another annotation

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -9,7 +9,7 @@ public class MyApp {
                 "applicationContext.xml");
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-        MessageService messageServiceAux = applicationContext.getBean("messageService", MessageService.class);
+        MessageService messageServiceAux = applicationContext.getBean("messageServiceRandom", MessageService.class);
 
         System.out.println(messageService.getMessage());
         System.out.println(messageServiceAux.getMessage());
--- src/main/java/com/example/RandomTextMessageService.java
+++ src/main/java/com/example/RandomTextMessageService.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import org.springframework.stereotype.Component;
+
+@Component("messageServiceRandom")
 public class RandomTextMessageService implements MessageService {
   private final String[] words = {
       "Spring",
